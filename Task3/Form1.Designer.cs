10f50dd0201fe240352dee3a9a8f4353a7820586
Nikolai Kochuk <ganortosys@gamil.com>
2022-12-20 19:30:35 +0500

    Add Task3 folder

---
+++ Task3/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cosCheck = new System.Windows.Forms.CheckBox();
+            this.sinCheck = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.chart, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.67615F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.32385F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(593, 420);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // chart
+            // 
+            chartArea2.AxisX.Title = "Ось X:";
+            chartArea2.AxisY.Title = "Ось Y:";
+            chartArea2.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea2);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            legend2.Title = "Функции:";
+            this.chart.Legends.Add(legend2);
+            this.chart.Location = new System.Drawing.Point(3, 3);
+            this.chart.Name = "chart";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Color = System.Drawing.Color.Maroon;
+            series3.Legend = "Legend1";
+            series3.LegendText = "sin(x)";
+            series3.Name = "sin(x)";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "cos(x)";
+            this.chart.Series.Add(series3);
+            this.chart.Series.Add(series4);
+            this.chart.Size = new System.Drawing.Size(587, 261);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "chart1";
+            title2.ForeColor = System.Drawing.Color.DarkOrange;
+            title2.Name = "Title1";
+            title2.Text = "Графики функций:";
+            this.chart.Titles.Add(title2);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.cosCheck);
+            this.groupBox1.Controls.Add(this.sinCheck);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 270);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(587, 147);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление:";
+            // 
+            // cosCheck
+            // 
+            this.cosCheck.AutoSize = true;
+            this.cosCheck.Checked = true;
+            this.cosCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cosCheck.Location = new System.Drawing.Point(118, 27);
+            this.cosCheck.Margin = new System.Windows.Forms.Padding(30, 30, 3, 3);
+            this.cosCheck.Name = "cosCheck";
+            this.cosCheck.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cosCheck.Size = new System.Drawing.Size(56, 17);
+            this.cosCheck.TabIndex = 4;
+            this.cosCheck.Text = "cos(X)";
+            this.cosCheck.UseVisualStyleBackColor = true;
+            this.cosCheck.Click += new System.EventHandler(this.OnCheckChange);
+            // 
+            // sinCheck
+            // 
+            this.sinCheck.AutoSize = true;
+            this.sinCheck.Checked = true;
+            this.sinCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.sinCheck.Location = new System.Drawing.Point(33, 27);
+            this.sinCheck.Margin = new System.Windows.Forms.Padding(30, 30, 3, 3);
+            this.sinCheck.Name = "sinCheck";
+            this.sinCheck.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.sinCheck.Size = new System.Drawing.Size(52, 17);
+            this.sinCheck.TabIndex = 3;
+            this.sinCheck.Text = "sin(X)";
+            this.sinCheck.UseVisualStyleBackColor = true;
+            this.sinCheck.Click += new System.EventHandler(this.OnCheckChange);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "-10,0";
+            this.textBox1.TextChanged += new System.EventHandler(this.OnUpdateChart);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Начало X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(411, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Конец X:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(468, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "10,0";
+            this.textBox2.TextChanged += new System.EventHandler(this.OnUpdateChart);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Амплитуда:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(275, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "1,0";
+            this.textBox3.TextChanged += new System.EventHandler(this.OnUpdateChart);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(397, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Частота:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(468, 63);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "1,0";
+            this.textBox4.TextChanged += new System.EventHandler(this.OnUpdateChart);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 420);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Рисование графиков";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cosCheck;
+        private System.Windows.Forms.CheckBox sinCheck;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Task3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WriteData(Func<double,double> fun,int index,float range = 1, float freq = 1,
+                                float start = -10, float end = 10,float step = 1.0f)
+        {
+            chart.Series[index].Points.Clear();
+            while (start < end)
+            {
+                double y = range*fun(freq*start);
+                chart.Series[index].Points.AddXY(start, y);
+                start += step;
+            }
+        }
+        
+        private void OnLoad(object sender, EventArgs e)
+        {
+            Func<double, double> fun = (p) =>
+            {
+                double offset = p > 0 ? -0.01 : 0.01;
+                return Math.Abs(p - Math.PI / 2) < 0.0001f ? Math.Tan(p+offset): Math.Tan(p);
+            };
+            WriteData(Math.Sin, 0);
+            WriteData(Math.Cos, 1);
+        }
+
+        private void OnCheckChange(object sender, EventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            int index = cb == sinCheck ? 0 : 1;
+            chart.Series[index].Enabled = cb.Checked;
+        }
+
+        private void OnUpdateChart(object sender, EventArgs e)
+        {
+            float value;
+            TextBox txt = sender as TextBox;
+            if (float.TryParse(txt.Text,out value))
+            {
+                TextBox[] tb = { textBox1, textBox2, textBox3, textBox4 };
+                float[] values = new float[4];
+                for (int i = 0; i < 4; ++i)
+                {
+                    if (!float.TryParse(tb[i].Text, out value))
+                        return;
+                    values[i] = value;
+                }
+                WriteData(Math.Sin, 0, values[2], values[3], values[0], values[1]);
+                WriteData(Math.Cos, 1, values[2], values[3], values[0], values[1]);
+            }
+        }
+    }
+}
